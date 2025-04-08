b196080959bb2ae8a637c2e3ec45d45a3895f4cd
Pablo Scolpino <pabloscolpino@gmail.com>
2025-04-07 21:02:05 -0300

    Remove log to stderr of positive message

--- backup.sh
+++ backup.sh
@@ -5,8 +5,6 @@ log() {
   echo "$@" >&2
 }
 
-log "Starting Cloudinary backup process"
-
 if [ -z "$CLOUDINARY_API_KEY" ]; then
   log "ERROR: CLOUDINARY_API_KEY is not set. Set this environment variable and try again."
   exit 1
@@ -25,16 +23,13 @@ fi
 export CLOUDINARY_URL=cloudinary://${CLOUDINARY_API_KEY}:${CLOUDINARY_API_SECRET}@${CLOUDINARY_CLOUD_NAME}
 
 BACKUP_DIR=/tmp/cloudinary_download
-log "Using backup directory: $BACKUP_DIR"
 
 # Pull cloudinary contents to local dir
-log "Pulling Cloudinary contents to local directory"
 cld sync --pull $BACKUP_DIR / 2>&2
 if [ $? -ne 0 ]; then
   log "ERROR: Failed to sync Cloudinary contents"
   exit 1
 fi
-log "Successfully pulled Cloudinary contents"
 
 # set -e
 
